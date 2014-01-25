dd61dc608e11dff5916943b60d1aa4fc86ab3885
Steffion <stefdegoey@gmail.com>
2014-01-25 14:06:38 +0100

    + Added API. (What it is for now.)

---
+++ src/nl/Steffion/AdminEye/AdminEyeAPI.java
@@ -0,0 +1,63 @@
+package nl.Steffion.AdminEye;
+
+import nl.Steffion.AdminEye.Commands.HPCommand;
+import nl.Steffion.AdminEye.Commands.KickCommand;
+import nl.Steffion.AdminEye.Commands.SlayCommand;
+
+import org.bukkit.entity.Player;
+
+public class AdminEyeAPI {
+
+	/**
+	 * Sets a player's health.
+	 * 
+	 * @param player
+	 *            Player who issued. null for unknown.
+	 * @param playerName
+	 *            Player's name who issued. "$" for console, "#" for 'system',
+	 *            or just the player's name.
+	 * @param healPlayerName
+	 *            Player's name you want to set the health of. "*" for everyone.
+	 * @param amount
+	 *            Amount of health. 0-20. Note: 0 is dead.
+	 */
+	public static void setHealth(Player player, String playerName,
+			String healPlayerName, String amount) {
+		HPCommand.setHealth(player, playerName, healPlayerName, amount);
+	}
+
+	/**
+	 * Kicks (disconnects) a player from the server.
+	 * 
+	 * @param player
+	 *            Player who issued. null if unknown.
+	 * @param playerName
+	 *            Player's name who issued. "$" for console, "#" for 'system',
+	 *            or just the player's name.
+	 * @param kickPlayerName
+	 *            Player's name you want to kick. "*" for everyone.
+	 * @param reason
+	 *            Reason for kick. null makes it "No reason given.". Colours are
+	 *            supported and use "&u" for next line.
+	 */
+	public static void kickPlayer(Player player, String playerName,
+			String kickPlayerName, String reason) {
+		KickCommand.kickPlayer(player, playerName, kickPlayerName, reason);
+	}
+
+	/**
+	 * Slays (kills) a player.
+	 * 
+	 * @param player
+	 *            Player who issued. null if unknown.
+	 * @param playerName
+	 *            Player's name who issued. "$" for console, "#" for 'system',
+	 *            or just the player's name.
+	 * @param slayPlayerName
+	 *            Player's name you want to slay. "*" for everyone.
+	 */
+	public static void slayPlayer(Player player, String playerName,
+			String slayPlayerName) {
+		SlayCommand.slayPlayer(player, playerName, slayPlayerName);
+	}
+}
