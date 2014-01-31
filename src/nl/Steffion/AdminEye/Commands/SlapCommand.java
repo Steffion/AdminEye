6349f3a3c2b06dd8868dfb24a950dcebf5c424d5
Steffion <stefdegoey@gmail.com>
2014-01-31 18:31:59 +0100

    ~ Made better slap effect.

--- src/nl/Steffion/AdminEye/Commands/SlapCommand.java
+++ src/nl/Steffion/AdminEye/Commands/SlapCommand.java
@@ -1,13 +1,17 @@
 package nl.Steffion.AdminEye.Commands;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 import nl.Steffion.AdminEye.AdminEye;
 import nl.Steffion.AdminEye.AdminEyeUtils;
 import nl.Steffion.AdminEye.StefsAPI;
 import nl.Steffion.AdminEye.StefsAPI.ExecutedCommand;
+
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class SlapCommand extends ExecutedCommand {
 	@Override
@@ -38,8 +42,12 @@ public class SlapCommand extends ExecutedCommand {
 		String slappedPlayers = "";
 
 		for (Player slapPlayer : slapPlayers) {
-
-			slapPlayer.damage(0);
+			Random random = new Random();
+			slapPlayer.setVelocity(new Vector((random.nextInt(4) + 1
+					- random.nextInt(9) + 1), (random.nextInt(2) + 1), (random
+					.nextInt(4) + 1 - random.nextInt(9) + 1)).multiply(0.5));
+			slapPlayer.playSound(slapPlayer.getLocation(), Sound.HURT_FLESH, 1,
+					1);
 			slappedPlayers += "%A" + slapPlayer.getName() + "%N, ";
 		}
 
